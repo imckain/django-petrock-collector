f33bc4e51d79264a58c1a102ed716516f504737e
imckain <ianmmckain@gmail.com>
2021-02-19 14:47:41 -0500

    added feeding model and M2M for hats and rocks

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
-class Petrock(models.Model):
-    name = models.CharField(max_length=250)
-    rockType = models.CharField(max_length=250)
-    description = models.TextField(max_length=250)
-    personality = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse("petrocks_detail", kwargs={"petrock_id": self.id})
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
 
 class Hat(models.Model):
     name = models.CharField(max_length=50)
@@ -24,5 +19,33 @@ class Hat(models.Model):
     def get_absolute_url(self):
         return reverse("hats_detail", kwargs={"pk": self.id})
 
+class Petrock(models.Model):
+    name = models.CharField(max_length=250)
+    rockType = models.CharField(max_length=250)
+    description = models.TextField(max_length=250)
+    personality = models.IntegerField()
+    hats = models.ManyToManyField(Hat)
 
+    def __str__(self):
+        return self.name
     
+    def get_absolute_url(self):
+        return reverse("petrocks_detail", kwargs={"petrock_id": self.id})
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    petrock = models.ForeignKey(Petrock, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
